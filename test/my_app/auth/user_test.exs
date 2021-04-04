9f44867bf58a52fdc0669eb5577cc25d57e875ef
Jim Cain <jim@spelt.io>
2021-04-03 23:29:01 -0400

    Add query test

--- test/my_app/auth/user_test.exs
+++ test/my_app/auth/user_test.exs
@@ -1,14 +1,32 @@
 defmodule MyApp.Auth.UserTest do
   use ExUnit.Case
+  import Seraph.Query
+
+  alias MyApp.Auth.User
 
   test "can create a User node" do
-    user = %MyApp.Auth.User{
+    user = %User{
       identifier: "phred",
       password: "foobar",
       email: "phred@example.net",
       name: "Phred"
     }
-    IO.inspect(user)
-    {:ok, user} = MyApp.Repo.Node.create(user)
+    assert {:ok, new_user} = MyApp.Repo.Node.create(user)
+    IO.inspect(new_user)
+  end
+
+  test "can query for a User node" do
+    original_user = %User{
+      identifier: "phred",
+      password: "foobar",
+      email: "phred@example.net",
+      name: "Phred"
+    }
+    assert {:ok, created_user} = MyApp.Repo.Node.create(original_user)
+
+    result = match([{u, User}])
+    |> where([u.identifier == "phred" and u.password == "foobar"])
+    |> return([u])
+    |> MyApp.Repo.one()
   end
 end
